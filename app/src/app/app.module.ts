4bf8b0684379e387b10d298b93387f386dd6b6fb
johniecheng <johniecheng@gmail.com>
2023-04-12 17:58:15 +0800

    View Encapsulation

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,12 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { SingleComponent } from './(A).lifecycle-hooks/1.single-component/single.component';
+import {AppComponent} from './app.component';
+import {SingleComponent} from './(A).lifecycle-hooks/1.single-component/single.component';
 import {FormsModule} from "@angular/forms";
-import { ParentComponent } from './(A).lifecycle-hooks/2.parent-child-component/parent/parent.component';
-import { ChildComponent } from './(A).lifecycle-hooks/2.parent-child-component/child/child.component';
-import { ChildContentComponent } from './(A).lifecycle-hooks/2.parent-child-component/child-content/child-content.component';
+import {ParentComponent} from './(A).lifecycle-hooks/2.parent-child-component/parent/parent.component';
+import {ChildComponent} from './(A).lifecycle-hooks/2.parent-child-component/child/child.component';
+import {
+  ChildContentComponent
+} from './(A).lifecycle-hooks/2.parent-child-component/child-content/child-content.component';
+import {NoEncapsulationComponent} from './(B).view-encapsulation/1.no-encapsulation/no-encapsulation.component';
+import {
+  EmulatedEncapsulationComponent
+} from './(B).view-encapsulation/2.emulated-encapsulation/emulated-encapsulation.component';
+import { ShadowDomEncapsulationComponent } from './(B).view-encapsulation/3.shadow-dom-encapsulation/shadow-dom-encapsulation.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +21,10 @@ import { ChildContentComponent } from './(A).lifecycle-hooks/2.parent-child-comp
     SingleComponent,
     ParentComponent,
     ChildComponent,
-    ChildContentComponent
+    ChildContentComponent,
+    NoEncapsulationComponent,
+    EmulatedEncapsulationComponent,
+    ShadowDomEncapsulationComponent
   ],
   imports: [
     BrowserModule,
@@ -23,4 +33,5 @@ import { ChildContentComponent } from './(A).lifecycle-hooks/2.parent-child-comp
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
